dec4ee7b4b36472b42c2c45a0dfc18874ba38ee1
chcuApp <github.achmadsfh03@gmail.com>
2024-05-31 21:49:07 +0700

    Add project files.

---
+++ BasicCalculator/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace BasicCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("masukan angka pertama: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("masukan angka kedua: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("masukan operator (+, -, *, /, %): ");
+            char op = Console.ReadKey().KeyChar;
+            Console.ReadLine(); //perlu klik enter sebelum program di eksekusi
+
+            double result = 0;
+
+            switch (op)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nzero is invalid division!");
+                        return;
+                    }
+                    break;
+                case '%':
+                    if (num2 != 0)
+                    {
+                        result = num1 % num2;
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nzero is invalid mod!");
+                        return;
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("\nunvalid operator");
+                    return;
+            }
+
+            Console.WriteLine($"\nResult = {num1} {op} {num2} = {result}");
+        }
+    }
+}
+
